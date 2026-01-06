39c4e6f0d22d0b8728cf4ac7761510441cea1218
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-01-07 03:08:24 +0700

    0.1: added recipes for some unobtainable (yet) dyes

---
+++ kubejs/server_scripts/misc_crafts.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.shapeless("2x minecraft:yellow_dye", ["#c:dyes/red", "#c:dyes/green"]);
+    event.shapeless("2x minecraft:brown_dye", ["#c:dyes/red", "#c:dyes/orange"]);
+});
