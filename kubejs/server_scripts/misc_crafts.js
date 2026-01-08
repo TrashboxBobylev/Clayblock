38520d01aecaa046f2eb69872248c775575ac7d3
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-01-09 02:47:39 +0700

    0.1: added ability to make crops with wheat and claysta fruits

--- kubejs/server_scripts/misc_crafts.js
+++ kubejs/server_scripts/misc_crafts.js
@@ -1,4 +1,29 @@
 ServerEvents.recipes(event => {
     event.shapeless("2x minecraft:yellow_dye", ["#c:dyes/red", "#c:dyes/green"]);
     event.shapeless("2x minecraft:brown_dye", ["#c:dyes/red", "#c:dyes/orange"]);
+
+    let crop_table = {
+        "orange" :"minecraft:carrot",
+        "yellow" :"minecraft:potato",
+        "brown"  :"minecraft:pumpkin_seeds",
+        "lime"   :"minecraft:melon_seeds",
+        "gray"   :"minecraft:bamboo",
+        "red"    :"minecraft:nether_wart",
+        "green"  :"minecraft:cactus",
+        "cyan"   :"minecraft:sugar_cane",
+        "blue"   :"minecraft:kelp",
+        "purple" :"minecraft:chorus_flower"
+    };
+
+    Object.keys(crop_table).forEach((value, index, array) => {
+        event.shaped(crop_table[value], [
+            "COC",
+            "CWC",
+            "COC"
+        ], {
+            "C": "kubejs:clay_fruit",
+            "W": "#c:crops/wheat",
+            "O": `minecraft:${value}_glazed_terracotta`
+        });
+    });
 });
