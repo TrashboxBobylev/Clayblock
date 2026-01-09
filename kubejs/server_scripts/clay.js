f80da7ce5e313462928662c8cf54a7cf1cae6a00
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-01-10 02:22:15 +0700

    0.1: added layers of terracotta into terracottaland

--- kubejs/server_scripts/clay.js
+++ kubejs/server_scripts/clay.js
@@ -2,6 +2,9 @@ ServerEvents.tags("block", event => {
     for (let clay of global.clays){
         event.add("minecraft:dirt", clay);
     }
+    for (let clay of global.terracottas){
+        event.add("minecraft:nether_carver_replaceables", clay);
+    }
     event.add("minecraft:saplings", "kubejs:clay_sapling");
 });
 
