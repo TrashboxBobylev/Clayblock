a2195e6250f68de9f2f1befbc9e81fa489c0cedc
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-01-13 00:31:01 +0700

    0.1: added clay energy generator

--- kubejs/server_scripts/clay.js
+++ kubejs/server_scripts/clay.js
@@ -11,3 +11,17 @@ ServerEvents.tags("block", event => {
 ServerEvents.tags("item", event => {
     event.add("minecraft:saplings", "kubejs:clay_sapling");
 });
+
+ServerEvents.recipes(event => {
+    let clay_fuels = [];
+    clay_fuels.push("minecraft:clay_ball");
+    global.clays.forEach(element => {
+        clay_fuels.push(element);
+    });
+    for (let i = 0; i < clay_fuels.length; i++){
+        let base_power = 500;
+        event.recipes.custommachinery.custom_machine(`custommachinery:clay_generator`, 10 * Math.pow(2, i))
+            .requireItem(clay_fuels[i], "fuel")
+            .produceEnergyPerTick(base_power*Math.pow(3, i) / (10 * Math.pow(2, i)));
+    }
+});
