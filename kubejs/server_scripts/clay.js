a9a6d5f2a0b6c54644cbb0a868bffe56c8132db5
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-01-13 03:47:19 +0700

    0.1: added advancements with some tips for progression

--- kubejs/server_scripts/clay.js
+++ kubejs/server_scripts/clay.js
@@ -25,3 +25,50 @@ ServerEvents.recipes(event => {
             .produceEnergyPerTick(base_power*Math.pow(3, i) / (10 * Math.pow(2, i)));
     }
 });
+
+ServerEvents.generateData("before_mods", event => {
+    for (let i = 1; i <= 8; i++){
+        event.json(`modpack:advancement/clay_${i}.json`, {
+            display: {
+                icon: {id: `kubejs:clay_${i}x`},
+                title: {translate: `modpack.advancement.clay_${i}.title`},
+                description: {translate: `modpack.advancement.clay_${i}.desc`}
+            },
+            criteria: {
+                clay: {
+                    trigger: "minecraft:inventory_changed",
+                    conditions: {
+                        items: [
+                            {items: `kubejs:clay_${i}x`}
+                        ]
+                    }
+                }
+            },
+            parent: `modpack:clay_${i-1}`,
+            rewards: {
+                experience: 2*(i+1)
+            }
+        });
+        event.json(`modpack:advancement/terracotta_${i}.json`, {
+            display: {
+                icon: {id: `kubejs:terracotta_${i}x`},
+                title: {translate: `modpack.advancement.terracotta_${i}.title`},
+                description: {translate: `modpack.advancement.terracotta_${i}.desc`}
+            },
+            criteria: {
+                terracotta: {
+                    trigger: "minecraft:inventory_changed",
+                    conditions: {
+                        items: [
+                            {items: `kubejs:terracotta_${i}x`}
+                        ]
+                    }
+                }
+            },
+            parent: `modpack:terracotta_${i-1}`,
+            rewards: {
+                experience: 4*(i+1)
+            }
+        });
+    }
+});
