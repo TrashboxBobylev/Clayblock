6c76ba10450efa724328980447db890711e59fd1
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-13 00:52:58 +0700

    0.1: fixed few issues with otherworldy crystal: - now can be properly mined - now can be silk-touched

--- kubejs/server_scripts/otherworldy_crystal.js
+++ kubejs/server_scripts/otherworldy_crystal.js
@@ -1,9 +1,19 @@
 LootJS.lootTables(event => {
     event.create("kubejs:blocks/otherworldy_crystal", LootType.BLOCK)
         .createPool(pool => {
-            pool.rolls([1, 2]);
+            let full_entry = LootEntry.alternative(LootEntry.of("kubejs:otherworldy_crystal").matchTool(
+                ItemFilter.hasEnchantment("minecraft:silk_touch", 1)
+            ));
+            let ores = [];
             for (let ore of ["antimony", "bauxite", "lead", "lignite_coal", "monazite", "nickel", "salt", "tin", "tungsten", "uranium"]){
-                pool.addEntry(LootEntry.of(`modern_industrialization:${ore}_dust`));
+                ores.push(LootEntry.of(`modern_industrialization:${ore}_dust`));
             }
+            full_entry.addEntry(LootEntry.group(ores));
+            pool.addEntry(full_entry);
         });
+});
+
+ServerEvents.tags("block", event => {
+    event.add("minecraft:mineable/pickaxe", "kubejs:otherworldy_crystal");
+    event.add("minecraft:needs_diamond_tool", "kubejs:otherworldy_crystal");
 });
