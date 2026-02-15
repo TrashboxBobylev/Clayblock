64bd0dfb2ef32160308df59e5e072e647716d1c9
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-16 00:19:23 +0700

    0.1: dragon fight now completely stops, if no players are alive

--- kubejs/server_scripts/dragon_thing.js
+++ kubejs/server_scripts/dragon_thing.js
@@ -1,3 +1,5 @@
+/** @type {typeof import("net.minecraft.server.level.ServerLevel").$ServerLevel } */
+let $ServerLevel  = Java.loadClass("net.minecraft.server.level.ServerLevel")
 EntityEvents.spawned(event => {
     if (event.entity.type == "minecraft:ender_dragon" && !event.server.persistentData.contains("first_dragon")){
         event.server.persistentData.putBoolean("first_dragon", true);
@@ -6,5 +8,32 @@ EntityEvents.spawned(event => {
             event.entity.kill();
             event.level.setBlock(new BlockPos(0, 66, 0), "minecraft:air", 2);
         });
+    }
+});
+
+EntityEvents.death("minecraft:player", event => {
+    if (event.level.dimension == "minecraft:the_end"){
+        /**
+         * @type {import("net.minecraft.server.level.ServerLevel").$ServerLevel$$Original}
+         */
+        let server_level = Java.cast($ServerLevel, event.level);
+        let dragon_info = server_level.getDragonFight();
+
+        if (dragon_info != null){
+            let are_players_alive = false;
+            for (let player of event.level.getPlayers()){
+                if (player.alive && player.distanceToSqr(new BlockPos(0, 128, 0)) <= 192*192){
+                    are_players_alive = true;
+                }
+            }
+            if (!are_players_alive){
+                event.server.scheduleInTicks(2, callback => {
+                    event.server.runCommandSilent("kill @e[type=minecraft:ender_dragon]");
+                    event.server.scheduleInTicks(1, callback => {
+                        event.level.setBlock(new BlockPos(0, 66, 0), "minecraft:air", 2);
+                    });
+                });
+            }
+        }
     }
 });
