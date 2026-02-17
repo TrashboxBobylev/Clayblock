dc74cc425316f75e0defc8e8e9cc0f28c21686ed
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-17 15:12:59 +0700

    0.1: made MI hidden until forge hammer is made

---
+++ kubejs/server_scripts/staging_mi_away.js
@@ -0,0 +1,25 @@
+AStages.customizeStage("MI")
+    .setAddChatMessage(Component.translatable("modpack.mi_unlocked").green())
+    .setAddSubTitle(Component.translatable("modpack.mi_title"))
+    .setStack("modern_industrialization:wrench");
+
+AStages.addRestrictionForMod("mi_away", "MI", "modern_industrialization")
+    .ignoreItems([
+        "modern_industrialization:forge_hammer",
+        "modern_industrialization:curium_block",
+        "modern_industrialization:curium_ingot",
+        "modern_industrialization:curium_dust",
+        "modern_industrialization:basalt_block",
+        "modern_industrialization:basalt_ingot",
+        "modern_industrialization:basalt_dust"
+    ]);
+
+AStages.addRestrictionForMod("mi_away_2", "MI", "extended_industrialization");
+
+AStages.addRestrictionForModRecipe("mi_away_3", "MI", "modern_industrialization");
+AStages.addRestrictionForModRecipe("mi_away_3", "MI", "extended_industrialization");
+
+PlayerEvents.advancement("modern_industrialization:forge_hammer", event => {
+    if (!AStages.playerHasStage("MI", event.player))
+        AStages.addStageToPlayer("MI", event.player);
+});
