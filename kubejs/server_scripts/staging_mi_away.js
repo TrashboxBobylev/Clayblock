5ae8cc10c34325983b379775062398031e49c0eb
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-17 19:28:03 +0700

    0.1: added industrialization overdrive to MI stage

--- kubejs/server_scripts/staging_mi_away.js
+++ kubejs/server_scripts/staging_mi_away.js
@@ -15,9 +15,11 @@ AStages.addRestrictionForMod("mi_away", "MI", "modern_industrialization")
     ]);
 
 AStages.addRestrictionForMod("mi_away_2", "MI", "extended_industrialization");
+AStages.addRestrictionForMod("mi_away_3", "MI", "industrialization_overdrive");
 
-AStages.addRestrictionForModRecipe("mi_away_3", "MI", "modern_industrialization");
-AStages.addRestrictionForModRecipe("mi_away_3", "MI", "extended_industrialization");
+AStages.addRestrictionForModRecipe("mi_away_4", "MI", "modern_industrialization");
+AStages.addRestrictionForModRecipe("mi_away_5", "MI", "extended_industrialization");
+AStages.addRestrictionForModRecipe("mi_away_6", "MI", "industrialization_overdrive");
 
 PlayerEvents.advancement("modern_industrialization:forge_hammer", event => {
     if (!AStages.playerHasStage("MI", event.player))
