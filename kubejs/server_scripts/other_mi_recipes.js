95560f273a7288cdbbb7ead8641f82c308f1d377
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-25 23:36:43 +0700

    0.1: buffed biodiesel and boosted diesel

--- kubejs/server_scripts/other_mi_recipes.js
+++ kubejs/server_scripts/other_mi_recipes.js
@@ -48,4 +48,23 @@ ServerEvents.recipes(event => {
     event.recipes.modern_industrialization.pressurizer(64, 800)
         .fluidIn("1000x modern_industrialization:heavy_water")
         .fluidOut("125x modern_industrialization:high_pressure_heavy_water");
+
+    event.remove("modern_industrialization:oil/mixer/sugar_solution");
+    event.recipes.modern_industrialization.mixer(2, 200)
+        .fluidIn("1000x minecraft:water")
+        .itemIn("3x minecraft:sugar")
+        .fluidOut("1000x modern_industrialization:sugar_solution");
+
+    event.remove("modern_industrialization:oil/chemical_reactor/plant_oil_to_raw_biodiesel");
+    event.recipes.modern_industrialization.chemical_reactor(12, 300)
+        .fluidIn("6000x modern_industrialization:plant_oil")
+        .fluidIn("400x modern_industrialization:ethanol")
+        .fluidIn("100x modern_industrialization:sodium_hydroxide")
+        .fluidOut("2000x modern_industrialization:raw_biodiesel");
+
+    event.remove("modern_industrialization:oil/chemical_reactor/ethanol_to_diethyl_ether");
+    event.recipes.modern_industrialization.chemical_reactor(20, 300)
+        .fluidIn("500x modern_industrialization:ethanol")
+        .fluidIn("25x modern_industrialization:acrylic_acid")
+        .fluidOut("400x modern_industrialization:diethyl_ether");
 });
