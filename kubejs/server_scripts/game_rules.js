f9bbe7806634fa3f04addd23ec4f663198513762
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-02-27 01:17:53 +0700

    0.1: made gamerule thing more consistent

--- kubejs/server_scripts/game_rules.js
+++ kubejs/server_scripts/game_rules.js
@@ -2,11 +2,10 @@ ServerEvents.loaded(event => {
     let {server} = event;
     if (!AStages.serverHasStage("first_load", server)){
         AStages.addStageToServer("first_load", server);
-        server.scheduleInTicks(20, callback => {
-            let gameRules = server.gameRules;
-            gameRules.set("doVillagerSummoning", "false");
-            gameRules.set("doInsomnia", "false");
-            gameRules.set("doTraderSpawning", "false");
+        server.scheduleInTicks(100, callback => {
+            server.runCommandSilent("gamerule doVillagerSummoning false");
+            server.runCommandSilent("gamerule doInsomnia false");
+            server.runCommandSilent("gamerule doTraderSpawning false");
         });
     }
 });
