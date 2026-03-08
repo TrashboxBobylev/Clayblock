6f92eeb9911fb09551dbf7f8e8d1bc29f9219e4a
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-03-08 22:44:20 +0700

    1.0: added 60 Pablo's tips

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -1,3 +1,13 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded client example script)')
 
+ClientEvents.generateAssets("before_mods", event => {
+    for (let i = 0; i < 60; i++){
+        event.json(`modpack:tips/tip_${i}.json`, {
+            type: "tipsmod:simple",
+            text: {
+                translate: `modpack.tip_${i}`
+            }
+        });
+    }
+});
